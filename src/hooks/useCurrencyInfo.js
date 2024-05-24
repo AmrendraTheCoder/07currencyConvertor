d096e98d6b4b6e8ba5f6ba0a4a0f05d4091fad79
Amrendra Vikram Singh <76041208+AmrendraTheCoder@users.noreply.github.com>
2024-05-25 03:23:59 +0530

    Update useCurrencyInfo.js

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -5,10 +5,26 @@ function useCurrencyInfo (currency) {
     useEffect( () => {
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => res.json())
-        .then((res) => setData(res[currency]))
-        console.log(data)
-    }, [currency])
-    return data
-}
+        .then((res) => {
+            const currencyData = res[currency]; // Get the object for the specified currency
+            const keysWithThreeDigitValues = Object.keys(currencyData).filter(key => {
+              return key.toString().length === 3; // Check if the value's length is 3
+            });
+            
+            // Create an object with filtered keys
+            const filteredData = keysWithThreeDigitValues.reduce((acc, key) => {
+              acc[key] = currencyData[key];
+              return acc;
+            }, {});
+    
+            setData(filteredData); // Update the state with the filtered data
+          })
+          .catch((error) => {
+            console.error('Error:', error); // Handle any errors
+          });
+      }, [currency]);
+    
+      return data;
+    }
 
-export default useCurrencyInfo;
+    export default useCurrencyInfo
